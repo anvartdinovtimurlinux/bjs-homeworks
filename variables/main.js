3627ede27f78d1af1df75d4f08dde4951258d0e1
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-11-06 07:31:25 +0300

    Домашнее задание к занятию 1.1 «Базовый синтаксис»

---
+++ variables/main.js
@@ -0,0 +1,28 @@
+// Задача №1
+console.log('------Задача №1------');
+
+let algebra = 3;
+let geography = 4;
+let physics = 3;
+
+let averageGrade = (algebra + geography + physics) / 3;
+console.log(`Средняя оценка за три предмета - ${averageGrade.toFixed(2)}`);
+
+
+// Задача №2
+console.log('------Задача №2------');
+
+let myName = 'Тимур';
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+
+// Задача №3
+console.log('------Задача №3------');
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
