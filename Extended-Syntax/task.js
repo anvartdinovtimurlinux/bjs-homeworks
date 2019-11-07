13b0e07f5e7be0fbaaca97733d674612fa38a252
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-11-07 08:15:33 +0300

    Домашнее задание к занятию 1.2 «Расширенный синтаксис»

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,40 +1,61 @@
+'use strict';
 
-
-function calculateQuadraticEquation(){
+function calculateQuadraticEquation() {
     let a = +window.a.value;
     let b = +window.b.value;
     let c = +window.c.value;
-    let result = getResult(a,b,c);
+    let result = getResult(a, b, c);
     window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
     let span = window.result;
-    span.textContent = "х = "+result;
+    span.textContent = "х = " + result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+function getResult(a, b, c) {
+    let d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+        return [];
+    } else if (d === 0) {
+        return [(-b + d ** 0.5) / (2 * a)];
+    } else {
+        return [(-b + d ** 0.5) / 2, (-b - d ** 0.5) / (2 * a)];
+    }
 }
 
-function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+function calculateAverageRating() {
+    let marks = window.marks.value.split("").map(Number).filter((n) => !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+function getAverageMark(marks) {
+    if (marks.length > 5) {
+        console.log('В массиве больше 5 оценок. Вычисление будет сделано по первым 5-и оценкам');
+        marks = marks.slice(0, 5);
+    }
+
+    let total_mark = 0;
+    for (let mark of marks) {
+        total_mark += mark;
+    }
+
+    return total_mark / marks.length;
 }
 
-function calculateDrinkTask(){
+function calculateDrinkTask() {
     let name = window.personName.value;
     let dateOfBirthday = new Date(window.dateOfBirthday.value);
     let drink = askDrink(name, dateOfBirthday);
     window.drink.textContent = drink;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+function askDrink(name, dateOfBirthday) {
+    let datet = (new Date) - dateOfBirthday;
+
+
+    let age = (new Date().getFullYear()) - dateOfBirthday.getFullYear();
+
+    if (age > 17) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    }
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
 }
