91b12516aeca4366c8ebdc541645284326849110
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-11-11 08:27:57 +0300

    Начало 4-ого ДЗ

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,28 +1,38 @@
-function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
-    let date = window.date.value;
+'use strict';
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
-    let span = window.mortageResult;
-    span.textContent = result;
+function calculateMortgage() {
+  let percent = window.percent.value;
+  let contribution = window.contribution.value;
+  let amount = window.amount.value;
+  let date = window.date.value;
+
+  let result = calculateTotalMortgage(percent, contribution, amount, date);
+  let span = window.mortageResult;
+  span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  for (let arg of arguments) {
+    console.log(arguments);
+    console.log(arg);
+    if (typeof Number(arg) !== 'number') {
+      
+      return `Параметр содержит неправильное значение ${arg}`;
+    }
+  }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+  // код для задачи №1 писать здесь
+  //return totalAmount;
 }
 
 function sayHello() {
-    let name = window.personName.value;
-    let greeting = getGreeting(name);
-    let span = window.helloResult;
-    span.textContent = greeting;
+  let name = window.personName.value;
+  let greeting = getGreeting(name);
+  let span = window.helloResult;
+  span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  // код для задачи №2 писать здесь
+  //return greeting;
 }
