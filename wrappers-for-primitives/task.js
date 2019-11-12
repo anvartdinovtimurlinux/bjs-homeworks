609d55e6399bee27eedf95716e314f3a035dfb87
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-11-12 08:23:59 +0300

    Домашнее задание к лекции 2.1 «Обертки для примитивов»

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -12,18 +12,34 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  for (let arg of arguments) {
-    console.log(arguments);
-    console.log(arg);
-    if (typeof Number(arg) !== 'number') {
-      
-      return `Параметр содержит неправильное значение ${arg}`;
+  const args = {
+    percent,
+    contribution,
+    amount
+  };
+  for (const key in args) {
+    if (isNaN(Number(args[key]))) {
+      const result = `Параметр ${key} содержит неправильное значение ${args[key]}`;
+      console.log(result);
+      return result;
     }
   }
 
-  // код для задачи №1 писать здесь
-  //return totalAmount;
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+  date = new Date(date);
+  const currentDate = new Date();
+  const months = date.getMonth() - currentDate.getMonth() + (12 * (date.getFullYear() - currentDate.getFullYear()));
+
+  const monthPercent = percent / (12 * 100);
+  const monthPay = (amount - contribution) * (monthPercent + monthPercent / (((1 + monthPercent) ** months) - 1));
+  const totalAmount = (monthPay * months).toFixed(2);
+
+  console.log(totalAmount);
+  return totalAmount;
 }
+
 
 function sayHello() {
   let name = window.personName.value;
@@ -33,6 +49,9 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-  // код для задачи №2 писать здесь
-  //return greeting;
+  name = name ? name : 'Аноним';
+
+  const greeting = `Привет, мир! Меня зовут ${name}.`;
+  console.log(greeting);
+  return greeting;
 }
